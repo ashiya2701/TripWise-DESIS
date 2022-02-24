933322f18d7473c538db380072990a64b37de82a
Tanveen <tanveen802@gmail.com>
2022-02-24 15:22:56 +0530

    Update models.py

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -22,4 +22,27 @@ class Flight(db.Model):
         dest= db.Column(db.Integer, ForeignKey(City.id))
         dur= db.Column(db.Float) 
         price =db.Column(db.Integer)
-       
+        
+class Bus(db.Model):
+        id=db.Column(db.Integer, primary_key=True)
+        src=db.Column(db.Integer, ForeignKey(City.id))
+        dest= db.Column(db.Integer, ForeignKey(City.id))
+        dur= db.Column(db.Float) 
+        price =db.Column(db.Integer) 
+
+class Trains(db.Model):
+        id=db.Column(db.Integer, primary_key=True)
+        src=db.Column(db.Integer, ForeignKey(City.id))
+        dest= db.Column(db.Integer, ForeignKey(City.id))
+        dur= db.Column(db.Float) 
+        price =db.Column(db.Integer)
+        
+class Hotels(db.Model):
+        id=db.Column(db.Integer, primary_key=True)
+        city=db.Column(db.Integer, ForeignKey(City.id))
+        price =db.Column(db.Integer)
+        distancefromairport =db.Column(db.Integer) 
+        distancefromrailways =db.Column(db.Integer) 
+        timefromairport =db.Column(db.Integer) 
+        timefromrailways =db.Column(db.Integer) 
+                
